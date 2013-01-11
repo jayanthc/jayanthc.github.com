efe32450279813dd29be37712db4f5726fd4868c
Jayanth Chennamangalam <jayanthc@gmail.com>
2013-01-10 18:18:55 -0800

    Initial commit.

---
+++ js/redirect.js
@@ -0,0 +1,12 @@
+/*
+ * redirect.js
+ * Redirect all visitors to www.jayanthchennamangalam.com
+ *
+ * Created by Jayanth Chennamangalam
+ */
+
+function redirect()
+{
+    window.location = "http://www.jayanthchennamangalam.com/";
+}
+
